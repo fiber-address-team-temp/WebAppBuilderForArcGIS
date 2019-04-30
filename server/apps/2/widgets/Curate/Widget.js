0219b1a081b241d40d63df972ef4553787f22644
Yan Zhang <zhayan@google.com>
2019-04-30 00:29:56 -0700

    optimize the code and change the delete store item only when post SUCCESS

--- server/apps/2/widgets/Curate/Widget.js
+++ server/apps/2/widgets/Curate/Widget.js
@@ -2,34 +2,22 @@ define([
     'dojo/on',
     'dojo/_base/declare',
     'dojo/_base/lang',
-    'dojo/_base/array',
     'dojo/dom-class',
     'dojo/json',
     'dojo/request',
-    'esri/request',
-    'esri/graphic',
     'dojo/Deferred',
     'jimu/BaseWidget',
     './jimu/LayerInfos/LayerInfos',
-    './jimu/LayerStructure',
     './jimu/dijit/TabContainer',
-    './jimu/utils',
-    "esri/tasks/query",
-    "esri/tasks/QueryTask",
-    'esri/tasks/Geoprocessor',
     'dijit/Tooltip',
     "dojox/grid/DataGrid",
     "dojox/grid/cells",
     "dojox/grid/cells/dijit",
     "dojo/store/Memory",
     "dojo/data/ObjectStore",
-    "dojo/date/locale",
-    "dojo/currency",
     "dijit/form/DateTextBox",
     "dijit/form/TextBox",
-    "dijit/form/HorizontalSlider",
     'dojo/data/ItemFileWriteStore',
-    'dojox/uuid/generateRandomUuid',
     'dijit/form/Select',
     "dijit/form/TextBox",
     "dijit/registry",
@@ -38,34 +26,22 @@ function(
     on,
     declare,
     lang,
-    array,
     domClass,
     json,
     request,
-    esriRequest,
-    Graphic,
     Deferred,
     BaseWidget,
     LayerInfos,
-    LayerStructure,
     TabContainer,
-    jimuUtils,
-    Query,
-    QueryTask,
-    Geoprocessor,
     Tooltip,
     DataGrid,
     cells,
     cellsDijit,
     Memory,
     ObjectStore,
-    locale,
-    currency,
     DateTextBox,
     TextBox,
-    HorizontalSlider,
     ItemFileWriteStore,
-    generateRandomUuid,
     Select,
     TextBox,
     registry
@@ -115,19 +91,37 @@ function(
 
     postCreate: function() {
         this.inherited(arguments);
-
         this._setLayers();
 
-        this.own(on(this.updateBtn, "click", lang.hitch(this, this._onCurationUpdate)));
-        this.own(on(this.deleteBtnUpdateTable, "click", lang.hitch(this, this._onCurationDeleteInUpdateTable)));
-        this.own(on(this.clearBtnUpdateTable, "click", lang.hitch(this, this._clearItemsInUpdateTable)));
+        // Update Addresses Table.
+        this.own(on(this.updateBtn, "click", lang.hitch(this, function(){
+          this._updateAddressesInSpanner(this.updateTableGrid.selection.getSelected(), this.GridName.UPDATE);
+        })));
+        this.own(on(this.deleteBtnUpdateTable, "click", lang.hitch(this, function(){
+          this._deleteAddressesInSpanner(this.updateTableGrid.selection.getSelected(), this.GridName.UPDATE);
+        })));
+        this.own(on(this.clearBtnUpdateTable, "click", lang.hitch(this, function(){
+          this._clearItems(this.GridName.UPDATE);
+        })));
 
-        this.own(on(this.createBtn, "click", lang.hitch(this, this._onCurationCreate)));
-        this.own(on(this.deleteBtnCreateTable, "click", lang.hitch(this, this._onCurationDeleteInCreateTable)));
-        this.own(on(this.clearBtnCreateTable, "click", lang.hitch(this, this._clearItemsInCreateTable)));
+        // Create Addresses Table.
+        this.own(on(this.createBtn, "click", lang.hitch(this, function(){
+          this._createAddressesInSpanner(this.updateTableGrid.selection.getSelected(), this.GridName.CREATE);
+        })));
+        this.own(on(this.deleteBtnCreateTable, "click", lang.hitch(this, function(){
+          this._deleteAddressesInSpanner(this.updateTableGrid.selection.getSelected(), this.GridName.CREATE);
+        })));
+        this.own(on(this.clearBtnCreateTable, "click", lang.hitch(this, function(){
+          this._clearItems(this.GridName.CREATE);
+        })));
 
-        this.own(on(this.addAddressBtnUpdate, "click", lang.hitch(this, this._addAddressToUpdateTableStore)));
-        this.own(on(this.addAddressBtnCreate, "click", lang.hitch(this, this._addAddressToCreateTableStore)));
+        // Create Single Address Entry.
+        this.own(on(this.addAddressBtnUpdate, "click", lang.hitch(this, function(){
+          this._addItem(this._fetchAddressItemInForm(), this.GridName.UPDATE);
+        })));
+        this.own(on(this.addAddressBtnCreate, "click", lang.hitch(this, function(){
+          this._addItem(this._fetchAddressItemInForm(), this.GridName.CREATE);
+        })));
 
         const TOOLTIP_CONTENT = "<div class='tooltipContent'>" +
             "<b>Curate</b> appends your selected uncurated addresses to the design address table.</div>";
@@ -260,22 +254,6 @@ function(
       this.selectedCountInUpdateTable.innerHTML = this.updateTableGrid.selection.getSelected().length;
     },
 
-    _onCurationUpdate: function(){
-        this._updateAddressesInSpanner(this._deleteSelectedItemsInTable(this.GridName.UPDATE))
-    },
-
-    _onCurationCreate: function(){
-        this._createAddressesInSpanner(this._deleteSelectedItemsInTable(this.GridName.CREATE))
-    },
-
-    _onCurationDeleteInUpdateTable: function(){
-        this._deleteAddressesInSpanner(this._deleteSelectedItemsInTable(this.GridName.UPDATE));
-    },
-
-    _onCurationDeleteInCreateTable: function(){
-        this._deleteAddressesInSpanner(this._deleteSelectedItemsInTable(this.GridName.CREATE));
-    },
-
     _fetchAddressItemInForm: function() {
       const globalIdValue = registry.byId("globalId").get("value");
       const cityValue = registry.byId("city").get("value");
@@ -300,17 +278,8 @@ function(
       return item
     },
 
-    _addAddressToUpdateTableStore: function() {
-      this._addItem(this._fetchAddressItemInForm(), this.GridName.UPDATE);
-    },
-
-    _addAddressToCreateTableStore: function() {
-      this._addItem(this._fetchAddressItemInForm(), this.GridName.UPDATE);
-    },
-
-    _deleteSelectedItemsInTable: function(gridName) {
+    _deleteSelectedItemsInTable: function(items, gridName) {
       if(gridName === this.GridName.UPDATE) {
-        const items = this.updateTableGrid.selection.getSelected();
         const updateTableDeletionCount = items.length
         console.log(`Delete ${updateTableDeletionCount} item(s) from store.`)
         if(updateTableDeletionCount){
@@ -333,17 +302,17 @@ function(
       return {};
     },
 
-    _clearItemsInUpdateTable: function() {
-      const updateTableAllCount = this.updateTableGrid.rowCount;
-      console.log(`Clear all ${updateTableAllCount} item(s) from store.`)
-      this.updateTableStore = new ItemFileWriteStore({data: {identifier: "GLOBALID", items: []}}),
-      this.updateTableGrid.setStore(this.updateTableStore);
-      this.recordCountInUpdateTable.innerHTML = 0;
-      this._disableBtns(this.GridName.UPDATE)
-    },
-
-    _clearItemsInCreateTable: function() {
-      // TODO(XXX): Implement it.
+    _clearItems: function(gridName) {
+      if(gridName === this.GridName.UPDATE) {
+        const updateTableAllCount = this.updateTableGrid.rowCount;
+        console.log(`Clear all ${updateTableAllCount} item(s) from store.`)
+        this.updateTableStore = new ItemFileWriteStore({data: {identifier: "GLOBALID", items: []}}),
+        this.updateTableGrid.setStore(this.updateTableStore);
+        this.recordCountInUpdateTable.innerHTML = 0;
+        this._disableBtns(this.GridName.UPDATE)
+      } else {
+        // TODO(XXX): Implement it.
+      }
     },
 
     _disableBtns: function(gridName) {
@@ -371,71 +340,72 @@ function(
     },
 
     _createAddressesInSpanner: function(items){
-        console.log("Create items:" + items)
-        console.log("Send CREATE request to one platform api.")
-        const getURL = "https://dog.ceo/api/breeds/image/random"
-        const postURL = "https://httpbin.org/post"
-        const postData = {items: items}
-        request.get(getURL, {
-            headers: {
-              "X-Requested-With": null
-            }
-        }).then(function(response){
-            console.log(`Test GET rest api: ${getURL}, Response: `, response);
-        });
-        request.post(postURL, {
-            data: postData,
-            headers: {
-              "X-Requested-With": null
-            }
-        }).then(function(response){
-            console.log(`Test POST rest api: ${postURL}, for POST data: ${json.stringify(postData)}, Response: `, response);
-        });
+      console.log("Create items in spanner:" + items)
+      const getURL = "https://dog.ceo/api/breeds/image/random"
+      const postURL = "https://httpbin.org/post"
+      const postData = {items}
+      request.get(getURL, {
+          headers: {
+            "X-Requested-With": null
+          }
+      }).then(lang.hitch(this, function(response) {
+          console.log(`Test GET rest api: ${getURL}, Response: `, response);
+      }));
+      request.post(postURL, {
+          data: postData,
+          headers: {
+            "X-Requested-With": null
+          }
+      }).then(lang.hitch(this, function(response) {
+        this._deleteSelectedItemsInTable(items, gridName);
+        console.log(`Test POST rest api: ${postURL}, for POST data: ${json.stringify(postData)}, Response: `, response);
+      }));
     },
 
-    _updateAddressesInSpanner: function(items){
-        console.log("Update items:" + items)
-        console.log("Send UPDATE request to one platform api.")
+    _updateAddressesInSpanner: function(items, gridName){
+        console.log("Update items in spanner:" + items)
         const getURL = "https://dog.ceo/api/breeds/image/random"
         const postURL = "https://httpbin.org/post"
-        const postData = {items: items}
+        const postData = {items}
         request.get(getURL, {
             headers: {
               "X-Requested-With": null
             }
-        }).then(function(response){
+        }).then(lang.hitch(this, function(response) {
             console.log(`Test GET rest api: ${getURL}, Response: `, response);
-        });
+        }));
         request.post(postURL, {
             data: postData,
             headers: {
               "X-Requested-With": null
             }
-        }).then(function(response){
-            console.log(`Test POST rest api: ${postURL}, for POST data: ${json.stringify(postData)}, Response: `, response);
-        });
+        }).then(lang.hitch(this, function(response) {
+          this._deleteSelectedItemsInTable(items, gridName);
+          console.log(`Test POST rest api: ${postURL}, for POST data: ${json.stringify(postData)}, Response: `, response);
+        }));
     },
 
-    _deleteAddressesInSpanner: function(items){
-        console.log("Send DELETE request to one platform api.")
-        const getURL = "https://dog.ceo/api/breeds/image/random"
-        const postURL = "https://httpbin.org/post"
-        const postData = {items: items}
-        request.get(getURL, {
-            headers: {
-              "X-Requested-With": null
-            }
-        }).then(function(response){
-            console.log(`Test GET rest api: ${getURL}, Response: `, response);
-        });
-        request.post(postURL, {
-            data: postData,
-            headers: {
-              "X-Requested-With": null
-            }
-        }).then(function(response){
-            console.log(`Test POST rest api: ${postURL}, for POST data: ${json.stringify(postData)}, Response: `, response);
-        });
+    _deleteAddressesInSpanner: function(items, gridName){
+      console.log("Delete items in spanner:" + items)
+      const getURL = "https://dog.ceo/api/breeds/image/random"
+      const postURL = "https://httpbin.org/post"
+      const postData = {items}
+      request.get(getURL, {
+          headers: {
+            "X-Requested-With": null
+          }
+      }).then(lang.hitch(this, function(response) {
+          console.log(`Test GET rest api: ${getURL}, Response: `, response);
+      }));
+      request.post(postURL, {
+          data: postData,
+          headers: {
+            "X-Requested-With": null
+          }
+      }).then(lang.hitch(this, function(response) {
+        this._deleteSelectedItemsInTable(items, gridName);
+        console.log(`Test POST rest api: ${postURL}, for POST data: ${json.stringify(postData)}, Response: `, response);
+      }));
     },
 
     _setLayers: function(){
